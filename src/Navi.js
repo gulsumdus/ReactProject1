8f68cf07937074bee249535af56ac99f38ea7d95
gulsumdus <dusgulsum@gmail.com>
2025-02-27 14:48:36 +0300

    first commit

---
+++ src/Navi.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+//Biz kendimiz bu componenti oluşturduk
+export default class Navi extends Component {
+  render() {
+    return (
+    <div>
+      <h2>Navi Component</h2>
+    </div>
+    )
+  }
+}
