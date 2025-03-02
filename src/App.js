a1d6d58e74fafc0c6ac3d653012ab9c40b8ec41c
gulsumdus <dusgulsum@gmail.com>
2025-03-02 17:43:48 +0300

    Commit 2
    -added api folder and I did some changes.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Navi from './Navi';
 import CategoryList from './CategoryList';
 import ProductList from './ProductList';
 import { Col, Container, Row } from 'reactstrap';// reactstrap paketini yükledik
-function App() {
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList />
-          </Col>
-          <Col xs="9">
-            <ProductList />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
 
 
+
+export default class App extends Component {
+
+  state={currentCategory:""}
+
+  changeCategory = ctgry=> {                              //fonksiyonlar değişken gibi kullanılır
+    this.setState({ currentCategory: ctgry.categoryName });
+  };
+
+  render() {
+    let productInfo = { title: "Product List" }// Encapsulation
+    let categoryInfo = { title: "Category List" }
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo} /> 
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+
+  }
 }
 
-export default App;
+
+
+
--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react'
-import { ListGroup, ListGroupItem} from 'reactstrap'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" }]
+        
+    }
+  }
+
+
   render() {
     return (
       <div>
-        <h3>CategoryList</h3>
+        <h1>{this.state.counter}</h1>
+        <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>
-            Cras justo odio
-          </ListGroupItem>
-          <ListGroupItem>
-            Dapibus ac facilisis in
-          </ListGroupItem>
-          <ListGroupItem>
-            Morbi leo risus
-          </ListGroupItem>
-          <ListGroupItem>
-            Porta ac consectetur ac
-          </ListGroupItem>
-          <ListGroupItem>
-            Vestibulum at eros
-          </ListGroupItem>
+          {this.state.categories.map(kategori => (
+            <ListGroupItem onClick={() => this.props.changeCategory(kategori)} key={kategori.categoryId}>
+              {kategori.categoryName}
+            </ListGroupItem>
+          ))}
+
         </ListGroup>
+
+        <h4>{this.props.currentCategory}</h4>
       </div>
     )
   }
 }
+
+//map: dizinin her elemanını tek tek işler.
+//Yeni bir dizi döndürür (ancak burada JSX render edildiği için bir bileşen listesi üretiliyor).
+//React'te, her eleman için unique bir key (key) verilmesi önerilir (bu, performansı artırır).
